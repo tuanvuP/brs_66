34bf74f848c14dc2ef4f63dc3fd6fd851089396a
NguyenThiHangB <nguyen.thi.hangb@framgia.com>
2018-07-25 15:32:56 +0700

    Register user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t ".success"
+      redirect_to root_path
+    else
+      flash[:danger] = t ".danger"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :username, :email, :password,
+      :password_confirmation
+  end
+end
