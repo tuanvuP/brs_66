3d621186af1c57b44c5c85e4cabc5d1e524abdc9
NguyenThiHangB <nguyen.thi.hangb@framgia.com>
2018-07-31 11:11:47 +0700

    Create comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :content, length: {minimum: Settings.comment.minimum,
+    maximum: Settings.comment.maximum}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  resources :users, only: %i(new create)
+  resources :users, only: %i(new create show)
   resources :books, only: %i(index show)
   resources :categories
+  resources :comments, only: %i(new create show)
 end
